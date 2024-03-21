74cf533a5f764abc9a561720ad80177c383ba6e0
Your Name <you@example.com>
2024-03-21 16:00:12 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -42,16 +42,15 @@ Al finalizar retorna la lista creada.
 */
 
 List* crea_lista() {
-  List* L = create_list();
+  List* list = create_list();
   int i;
   for(i=1;i<=10;i++)
     {
       int* dato = (int*)malloc(sizeof(int));
       *dato = i;
-      pushBack(L, dato);
-    }
-  
-  return L;
+      pushBack(list, dato);
+    }  
+  return list;
 }
 
 /*
@@ -60,7 +59,8 @@ Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
-   return 0;
+  
+  return 0;
 }
 
 /*
